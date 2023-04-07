01777839549a06967aa50c0c6ff75618af51e662
Tomer <tomerrim@gmail.com>
2023-04-07 20:13:48 +0300

    initialize db and create Add Category

---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            //custom validation that makes sure that the name and the display order are not the same
+            if(category.Name == category.DisplayOrder.ToString())
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                 return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
