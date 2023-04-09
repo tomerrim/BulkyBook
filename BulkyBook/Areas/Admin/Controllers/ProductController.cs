2d59020ec17236f92a65a431bafba2136ef7ece1
Tomer <tomerrim@gmail.com>
2023-04-09 21:11:16 +0300

    add CRUD to Product and make category to be forign key to product

---
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,94 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+        public IActionResult Index()
+        {
+            var objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> categoryList = _unitOfWork.Category.GetAll()
+               .Select(c => new SelectListItem
+               {
+                   Text = c.Name,
+                   Value = c.Id.ToString()
+               });
+            ViewBag.CategoryList = categoryList;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+            if (productFromDb == null)
+                return NotFound();
+
+            return View(productFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+            if (productFromDb == null)
+                return NotFound();
+
+            return View(productFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+            if (productFromDb == null)
+                return NotFound();
+
+            _unitOfWork.Product.Remove(productFromDb);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
