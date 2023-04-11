f3d3a8d3031f7d5dad96736161869b5af79db55a
Tomer <tomerrim@gmail.com>
2023-04-11 20:38:16 +0300

    register company user

--- BulkyBook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyBook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -172,6 +172,12 @@ namespace BulkyBook.Areas.Identity.Pages.Account
                 user.State = Input.State;
                 user.PhoneNumber = Input.PhoneNumber;
                 user.Name = Input.Name;
+
+                if (Input.Role == SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
