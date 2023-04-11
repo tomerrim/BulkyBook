254c0898322ce059cb7f626078632fddca43ea86
Tomer <tomerrim@gmail.com>
2023-04-11 16:30:04 +0300

    add more props to user and add 4 roles

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace BulkyBook.DataAccess.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) { }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
