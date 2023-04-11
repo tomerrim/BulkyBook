6de0f59deb26521f75bca2bacf0f9bca07814313
Tomer <tomerrim@gmail.com>
2023-04-11 18:04:32 +0300

    add more fields to register page

--- BulkyBook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyBook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -109,6 +109,14 @@ namespace BulkyBook.Areas.Identity.Pages.Account
 
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string StreetAddress { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string PostalCode { get; set; }
+            public string PhoneNumber { get; set; }
         }
 
 
@@ -145,6 +153,12 @@ namespace BulkyBook.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.PostalCode = Input.PostalCode;
+                user.State = Input.State;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.Name = Input.Name;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
