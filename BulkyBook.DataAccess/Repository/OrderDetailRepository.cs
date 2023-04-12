f90807451591091d99db5445a50f6be9a3ee40ca
Tomer <tomerrim@gmail.com>
2023-04-12 17:25:55 +0300

    added OrderHeader and OrderDetail to db and to the repository

---
+++ BulkyBook.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db) => _db = db;
+        void IOrderDetailRepository.Update(OrderDetail orderDetail) => _db.OrderDetails.Update(orderDetail);
+    }
+}
