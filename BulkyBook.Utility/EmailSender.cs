ad289e22832f48f5412136e040e6443edc7c27b9
Tomer <tomerrim@gmail.com>
2023-04-15 15:59:56 +0300

    added SendGrid to send Emails

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +12,20 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+        public EmailSender(IConfiguration config)
+        {
+            SendGridSecret = config.GetValue<string>("SendGrid:SecretKey");
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             //logic to send email
-            return Task.CompletedTask;
+            var client = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress("tomerrim@gmail.com","Bulky Book");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject,"",htmlMessage);
+            return client.SendEmailAsync(message);
         }
     }
 }
--- BulkyBook/Areas/Customer/Controllers/CartController.cs
+++ BulkyBook/Areas/Customer/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Stripe.Checkout;
 using Stripe.FinancialConnections;
@@ -18,12 +19,14 @@ namespace BulkyBook.Areas.Customer.Controllers
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
 
         [BindProperty]
         public ShoppingCartVM ShoppingCartVM { get; set; }
-        public CartController(IUnitOfWork unitOfWork)
+        public CartController(IUnitOfWork unitOfWork,IEmailSender emailSender)
         {
             _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
         }
         public IActionResult Index()
         {
@@ -176,6 +179,8 @@ namespace BulkyBook.Areas.Customer.Controllers
 				}
                 HttpContext.Session.Clear();
 			}
+            _emailSender.SendEmailAsync(orderHeader.ApplicationUser.Email, "New Order - Bulky Book",
+                $"<p>New Order Created - {orderHeader.Id}</p>");
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
             _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
