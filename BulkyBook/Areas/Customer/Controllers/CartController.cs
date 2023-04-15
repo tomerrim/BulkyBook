e34dfbfbc20209d1ad006a037d066e0c0176c71e
Tomer <tomerrim@gmail.com>
2023-04-15 13:40:18 +0300

    make only the admin and employee access to the dropdown on register page

--- BulkyBook/Areas/Customer/Controllers/CartController.cs
+++ BulkyBook/Areas/Customer/Controllers/CartController.cs
@@ -174,6 +174,7 @@ namespace BulkyBook.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
 				}
+                HttpContext.Session.Clear();
 			}
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
